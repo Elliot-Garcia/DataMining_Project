c1f07b260b10f573189b4016d18e6e22812e2040
Elliot-Garcia <elliotgarciabrewer@gmail.com>
2022-03-16 15:38:27 +0100

    Fin couleurs 1 et 2

--- acquisition.py
+++ acquisition.py
@@ -17,35 +17,28 @@ df.to_json('pokemon.json')
 """Couleur prédominant image"""
 
 def CouleursClusters(numarray):
-    clusters = KMeans(n_clusters = 4)
+    clusters = KMeans(n_clusters = 3)
     clusters.fit(numarray)
-    npbins = numpy.arange(0, 5)
+    npbins = numpy.arange(0, 4)
     histogram = numpy.histogram(clusters.labels_, bins=npbins)
-    labels = numpy.unique(clusters.labels_)
-    barlist = plot.bar(labels, histogram[0])
-    return clusters, barlist
+    
+    histogram[0][np.where(histogram[0] == max(histogram[0]))] = 0
+    indexC1 = np.where(histogram[0] == max(histogram[0]))
+    histogram[0][np.where(histogram[0] == max(histogram[0]))] = 0
+    indexC2 = np.where(histogram[0] == max(histogram[0]))
+    return clusters, indexC1[0][0], indexC2[0][0]
 
-def CouleursRGBA(clusters, barlist):
-    for i in range(3):
-        couleur1='#%02x%02x%02x' % (
-        math.ceil(clusters.cluster_centers_[i][0]), 
-            math.ceil(clusters.cluster_centers_[i][1]),
-        math.ceil(clusters.cluster_centers_[i][2]))
-    #    barlist[i].set_color('#%02x%02x%02x' % (
-    #    math.ceil(clusters.cluster_centers_[i][0]), 
-    #        math.ceil(clusters.cluster_centers_[i][1]),
-    #    math.ceil(clusters.cluster_centers_[i][2])))
-    #plot.show()
-    return couleur1
+def CouleursRGBA(clusters, indexC):
+    couleur='#%02x%02x%02x' % (
+    math.ceil(clusters.cluster_centers_[indexC][0]), 
+        math.ceil(clusters.cluster_centers_[indexC][1]),
+    math.ceil(clusters.cluster_centers_[indexC][2]))
+    return couleur
 
-def CouleursP(clusters, barlist):
-    for i in range(3):
-        couleur1='#%02x0000' % (
-        math.ceil(clusters.cluster_centers_[i][0]))
-    #    barlist[i].set_color('#%02x0000' % (
-    #    math.ceil(clusters.cluster_centers_[i][0])))
-    #plot.show()
-    return couleur1
+def CouleursP(clusters, indexC):
+    couleur='#%02x0000' % (
+    math.ceil(clusters.cluster_centers_[indexC][0]))
+    return couleur
 
 def CouleurPokemon(imgName):
     try:
@@ -55,15 +48,17 @@ def CouleurPokemon(imgName):
 
     if (imgfile.mode == 'RGBA' or imgfile.mode == 'RGB'):
         numarray = numpy.array(imgfile.getdata(), numpy.uint8)
-        clusters, barlist = CouleursClusters(numarray)
-        couleur1=CouleursRGBA(clusters, barlist)
+        clusters, indexC1, indexC2 = CouleursClusters(numarray)
+        couleur1=CouleursRGBA(clusters, indexC1)
+        couleur2=CouleursRGBA(clusters, indexC2)
         
     if (imgfile.mode == 'P'):
         numarray = numpy.array(imgfile.getdata(), numpy.uint8).reshape(-1,1)
-        clusters, barlist = CouleursClusters(numarray)
-        couleur1=CouleursP(clusters, barlist)
+        clusters, indexC1, indexC2 = CouleursClusters(numarray)
+        couleur1=CouleursP(clusters, indexC1)
+        couleur2=CouleursP(clusters, indexC2)
     
-    return couleur1
+    return couleur1, couleur2
 
 
 """Ajout couleur prédominantes aux infos sur les images"""
@@ -74,5 +69,9 @@ dfPokemon.insert(3,"Couleur1", None)
 dfPokemon.insert(4,"Couleur2", None)
 
 for i in range(len(dfPokemon)): 
-    dfPokemon.loc[i,"Couleur1"] = CouleurPokemon(dfPokemon.loc[i,"Name"])
-    print("" + str(i) + " : " + dfPokemon.loc[i,"Couleur1"] + "")
+    couleur1, couleur2 = CouleurPokemon(dfPokemon.loc[i,"Name"])
+    dfPokemon.loc[i,"Couleur1"] = couleur1
+    dfPokemon.loc[i,"Couleur2"] = couleur2
+    print(str(i) + " : " + dfPokemon.loc[i,"Couleur1"] + " | " + dfPokemon.loc[i,"Couleur2"])
+
+dfPokemon.to_json('pokemon.json', orient="records")
